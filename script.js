2fc3f741ec6777324383658ebd22d93b106da822
sxmpra777 <p6026373@gmail.com>
2024-12-30 02:04:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function comprar(produto) {
+    alert(`Você escolheu comprar: ${produto}. Redirecionando para a página de pagamento.`);
+    // Substitua o URL abaixo pelo link da sua página de pagamento
+    window.location.href = "https://checkout.perfectpay.com.br/pay/PPU38CPCAJC?";
+
+}
